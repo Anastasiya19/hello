d582fe997ae666650f4a6d3fac1252e4cecd34f5
Irina Martyshko <i.masrtyshko@dunice.net>
2017-11-21 12:59:36 +0300

    added click by Enter

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -8,81 +8,84 @@
 //=  ../../bower_components/handlebars/handlebars.min.js
 
 function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 
 new WOW().init();
 $('.show-8-X').on('click', function () {
-    $('.browser__ex_1').css('display', 'block').addClass('animated fadeInDown');
+  $('.browser__ex_1').css('display', 'block').addClass('animated fadeInDown');
 });
 $(document).ready(function () {
-    // $('.send__input').on('input keyup', function() {
-    //     $('.helpers').delay(200).show();
-    // });
+  // $('.send__input').on('input keyup', function() {
+  //     $('.helpers').delay(200).show();
+  // });
 
-    $('.helpers__item').on('click', function () {
-        $('.helpers').slideToggle(200);
-        var value = $(this).html();
-        var input = $('.send__input');
-        input.val(value);
-    });
-    $('.scrollable-container-two').mCustomScrollbar({
-        theme: "my-theme",
-        setLeft: 15,
-        axis: "y",
-    });
-    var widthContaner = 0;
-    if ($('.message__item_horizontal').length > 1) {
-        for (var i = 0; i < $('.message__item_horizontal').length; i++) {
-            widthContaner += $('.message__item_horizontal').width();
-        }
-        $('.message__horizontal_scroll').css('width', widthContaner + 250);
+  $('.helpers__item').on('click', function () {
+    $('.helpers').slideToggle(200);
+    var value = $(this).html();
+    var input = $('.send__input');
+    input.val(value);
+  });
+  $('.scrollable-container-two').mCustomScrollbar({
+    theme: "my-theme",
+    setLeft: 15,
+    axis: "y",
+  });
+  var widthContaner = 0;
+  if ($('.message__item_horizontal').length > 1) {
+    for (var i = 0; i < $('.message__item_horizontal').length; i++) {
+      widthContaner += $('.message__item_horizontal').width();
     }
-    $('.speaking__close').click(function () {
-        $('.speaking__overlay').fadeOut(200)
-    })
+    $('.message__horizontal_scroll').css('width', widthContaner + 250);
+  }
+  $('.speaking__close').click(function () {
+    $('.speaking__overlay').fadeOut(200)
+  })
 
 
 });
 $('.macbook__play').on('click', function () {
-    var $video = $('.macbook__video'),
-        src = $('#myvideo').attr("src");
-    $video.html('<iframe id="myvideo" class="macbook__iframe" src="https://www.youtube.com/embed/uT3SBzmDxGk?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-    $video.css('display', 'block');
-    $('.macbook__content').hide();
+  var $video = $('.macbook__video'),
+    src = $('#myvideo').attr("src");
+  $video.html('<iframe id="myvideo" class="macbook__iframe" src="https://www.youtube.com/embed/uT3SBzmDxGk?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+  $video.css('display', 'block');
+  $('.macbook__content').hide();
 })
 $(window).on('load resize', function () {
-    var heightInfo = $('.view__info').innerHeight();
-    var heightPrice = $('.view__prices').innerHeight();
-    if ($(window).width() >= 768) {
-        if (heightInfo < heightPrice) {
-            $('.view__prices, .view__info-full').css('height', heightInfo + 'px');
-        }
-    } else {
-        $('.view__prices, .view__info-full').css('height', 'auto');
+  var heightInfo = $('.view__info').innerHeight();
+  var heightPrice = $('.view__prices').innerHeight();
+  if ($(window).width() >= 768) {
+    if (heightInfo < heightPrice) {
+      $('.view__prices, .view__info-full').css('height', heightInfo + 'px');
     }
-    if ($(window).width() < 560) {
-        var width = $(window).width() - 70;
-        $('.width-full-mobile').css('width', width);
-    }
+  } else {
+    $('.view__prices, .view__info-full').css('height', 'auto');
+  }
+  if ($(window).width() < 560) {
+    var width = $(window).width() - 70;
+    $('.width-full-mobile').css('width', width);
+  }
+  blockAligner.setEqHeight($('.specific'), $('.specific__item'));
 });
 
-
+$(window).resize(function () {
+  blockAligner.setEqHeight($('.specific'), $('.specific__item'));
+})
 //= custom/animations.js
 //= custom/collapsing.js
 //= custom/selectLng.js
 //= custom/speakVinci.js
 $(window).on('load', function () {
-    $('.preloader').delay(1500).fadeOut('slow');
-    $('.preloader__logo').delay(1500).addClass('preloader__logo_top');
-    $('body').delay(1500);
+  $('.preloader').delay(1500).fadeOut('slow');
+  $('.preloader__logo').delay(1500).addClass('preloader__logo_top');
+  $('body').delay(1500);
 });
 
 /**
@@ -93,37 +96,42 @@ var mobiles_requested = [];
 var suggested_zlatans = [];
 
 $('#send_chat').on('click', function (event) {
-    console.log('sending message')
-    event.preventDefault();
-    var userAvatar = '/assets/build/assets/images/user-mes.svg';
+  event.preventDefault();
+  sendMessage()
+});
+$('.send__input').keypress(function (e) {
+  if (e.which == 13) {
+    sendMessage()
+  }
+});
+function sendMessage() {
+  var userAvatar = '/assets/build/assets/images/user-mes.svg';
 
-    var chat_input;
+  var chat_input;
 
-    if ($('.chat__bottom input').val() !== '') {
+  if ($('.chat__bottom input').val() !== '') {
 
-        $('.chat__messages')
-            .append(
-            `<div class="message message_sender">
+    $('.chat__messages')
+      .append(
+        `<div class="message message_sender">
                             <img class="user-logo" src="/assets/build/assets/images/user-mes.svg" alt="">
                             <div class="message__item message__item_user br-chat ">
                                 <p>${$('.chat__bottom input').val()}</p>
                             </div>
                 </div>`
-            );
+      );
 
-        // Store the value in chat_input variable
-        chat_input = $('.chat__bottom input').val();
+    // Store the value in chat_input variable
+    chat_input = $('.chat__bottom input').val();
 
-        // Clear Input & scroll Chat window-to last message
-        $('.chat__bottom input').val('');
-        $('.chat__messages').scrollTop($('.chat__messages')[0].scrollHeight);
-    }
+    // Clear Input & scroll Chat window-to last message
+    $('.chat__bottom input').val('');
+    $('.chat__messages').scrollTop($('.chat__messages')[0].scrollHeight);
+  }
 
-    // sending the message
-    hellovinciai(chat_input);
-
-
-});
+  // sending the message
+  hellovinciai(chat_input);
+}
 
 //= custom/carousel.js
 
@@ -151,8 +159,8 @@ $('#send_chat').on('click', function (event) {
 $(document).ready(function () {
 
 
-if (getParameterByName("q")) {
+  if (getParameterByName("q")) {
     $('.chat__bottom input').val(getParameterByName("q"))
     $('#send_chat').click()
-}
+  }
 })
