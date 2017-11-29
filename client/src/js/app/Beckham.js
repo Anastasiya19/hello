87fc065387ab2a80cc88409a828346c008c3a5a2
Muhammed Abozaid <aapozaid@gmail.com>
2017-11-29 12:17:17 +0200

    debugging

--- client/src/js/app/Beckham.js
+++ client/src/js/app/Beckham.js
@@ -185,7 +185,7 @@ Beckham.prototype.build_video_review_element = function(zlatan){
      // Insert the HTML code into the page
     $('.chat__messages').append(this.html);
 
-    init_specifications($(".chat__messages").children().last());
+    // init_specifications($(".chat__messages").children().last());
     
     this.scroll_into_view();
 
