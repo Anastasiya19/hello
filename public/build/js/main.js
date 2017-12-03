de70728131787908a59b4e518796afc5f091fe98
Muhammed Abozaid <aapozaid@gmail.com>
2017-12-03 15:30:13 +0200

    creating the element

--- public/build/js/main.js
+++ public/build/js/main.js
@@ -21104,6 +21104,25 @@ function get_more(event) {
     $.post("/get_more", { query_object: query, skip: skip }, function (mobiles) {
 
         console.log(mobiles)
+        //This constructor is meant for the query_status
+        //Beckham only does query routing and calls the necessary functions to create the request
+        var beckham = new Beckham(300);
+
+        //Zlatan takes cares of the response returned and calls respective functions to 
+        //create the context objects
+        var zlatan = new Zlatan(mobiles);
+
+        //Builds the text reply element
+        var buffon = new Buffon("Here is more");
+
+
+
+
+
+        //Check the status of the request to decide which element to create
+        beckham_router(beckham, zlatan, buffon);
+
+
 
     })
 
