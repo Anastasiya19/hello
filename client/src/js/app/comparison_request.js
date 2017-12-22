7867c65ea091930a789a2210f6acc517449c4883
Ankur Dhamija <ankurdhamija83@gmail.com>
2017-12-22 15:28:04 +0530

    Updated comparison element

--- client/src/js/app/comparison_request.js
+++ client/src/js/app/comparison_request.js
@@ -32,126 +32,17 @@ function comparison_request(zlatan, query_status){
 
 	if(query_status === 200){
 
-		var camera_row = {
-			header: "Camera",
-			other: []
-		}
+		giggs.camera_element();
+		giggs.battery_element();
+		giggs.sim_element();
+		giggs.display_element();
+		giggs.os_element();
+		giggs.ram_element();
+		giggs.connectivity_element();
+		giggs.sensors_element();
+		giggs.warranty_element();
+		giggs.dimensions_element();
 
-		camera_row.other.push(giggs.create_context("Primary camera", "camera", "primary_camera_pixels"));
-		camera_row.other.push(giggs.create_context("Selfie camera", "camera", "secondary_camera_pixels"));
-		camera_row.other.push(giggs.create_context("Video recording", "camera", "video_recording_status"));
-		camera_row.other.push(giggs.create_context("Video recording resolution", "camera", "video_recording_resolution"));
-		camera_row.other.push(giggs.create_context("HD Recording", "camera", "hd_recording_status"));
-		camera_row.other.push(giggs.create_context("Full HD Recording", "camera", "full_hd_recording_status"));
-		camera_row.other.push(giggs.create_context("Flash", "camera", "flash"));
-
-		var battery_row = {
-			header: "Battery",
-			other: []
-		}
-
-		battery_row.other.push(giggs.create_context("Battery capacity", "battery_power", "battery_capacity"));
-		battery_row.other.push(giggs.create_context("Battery type", "battery_power", "battery_type"));
-		battery_row.other.push(giggs.create_context("Removable battery status", "battery_power", "removable_battery_status"));
-
-
-		var sim_row = {
-			header: "Sim card",
-			other: []
-		}
-
-		sim_row.other.push(giggs.create_context("Sim type", "sim_features", "sim_type"));
-		sim_row.other.push(giggs.create_context("Sim size", "sim_features", "sim_size"));
-		sim_row.other.push(giggs.create_context("Hybrid Sim status", "sim_features", "hybrid_sim_slot_status"));
-
-		var display_row = {
-			header: "Display",
-			other: []
-		}
-
-		display_row.other.push(giggs.create_context("Screen size", "display_specifications", "display_size"));
-		display_row.other.push(giggs.create_context("Display type", "display_specifications", "display_type"));
-		display_row.other.push(giggs.create_context("Display colors", "display_specifications", "display_colors"));
-		display_row.other.push(giggs.create_context("Resolution", "display_specifications", "resolution"));
-		display_row.other.push(giggs.create_context("Resolution type", "display_specifications", "resolution_type"));
-
-		var os_row = {
-			header: "OS",
-			other: []
-		}
-
-		os_row.other.push(giggs.create_context("Operating system", "os_processor", "operating_system"));
-		os_row.other.push(giggs.create_context("Processor speed", "os_processor", "processor_speed"));
-		os_row.other.push(giggs.create_context("Processor core", "os_processor", "processor_core"));
-		os_row.other.push(giggs.create_context("Processor type", "os_processor", "processor_type"));
-
-
-		var ram_row = {
-			header: "RAM",
-			other: []
-		}
-
-		ram_row.other.push(giggs.create_context("RAM", "memory_storage", "ram"));
-
-		var connectivity_row = {
-			header: "Connectivity",
-			other: []
-		}
-
-		connectivity_row.other.push(giggs.create_context("Supported networks", "connectivity", "supported_networks"));
-		connectivity_row.other.push(giggs.create_context("Wifi", "connectivity", "wifi"));
-		connectivity_row.other.push(giggs.create_context("Bluetooth", "connectivity", "bluetooth_support"));
-		connectivity_row.other.push(giggs.create_context("Bluetooth version", "connectivity", "bluetooth_version"));
-		connectivity_row.other.push(giggs.create_context("USB connectivity", "connectivity", "usb_connectivity"));
-		connectivity_row.other.push(giggs.create_context("Audio jack", "connectivity", "audio_jack_width"));
-		connectivity_row.other.push(giggs.create_context("Near Field Communication (NFC)", "connectivity", "nfc"));
-
-
-
-
-		var sensors_row = {
-			header: "Sensors",
-			other: []
-		}
-
-		sensors_row.other.push(giggs.create_context("Sensors", "sensors"));
-
-		var warranty_row = {
-			header: "Warranty",
-			other: []
-		}
-
-
-		warranty_row.other.push(giggs.create_context("Warranty", "warranty"));
-
-
-
-		var dimensions_row = {
-			header: "Dimensions",
-			other: []
-		}
-
-
-		dimensions_row.other.push(giggs.create_context("Weight", "dimensions", "weight"));
-		dimensions_row.other.push(giggs.create_context("Width", "dimensions", "width"));
-		dimensions_row.other.push(giggs.create_context("Height", "dimensions", "height"));
-		dimensions_row.other.push(giggs.create_context("Depth", "dimensions", "depth"));
-
-		giggs.comparison_context.push(camera_row, battery_row, sim_row, display_row, os_row, ram_row, connectivity_row, sensors_row, warranty_row, dimensions_row);
-
-
-
-		//get the contexts of the mobiles
-		// giggs.create_context("Screen size", "display_specifications", "display_size");
-		// giggs.create_context("Sim size", "sim_features", "sim_size");
-		// giggs.create_context("Primary camera", "camera", "primary_camera_pixels");
-		// giggs.create_context("Selfie camera", "camera", "secondary_camera_pixels");
-		// giggs.create_context("Battery capacity", "battery_power", "battery_capacity");
-		// giggs.create_context("Operating system", "os_processor", "operating_system");
-		// giggs.create_context("Processor speed", "os_processor", "processor_speed");
-		// giggs.create_context("RAM", "memory_storage", "ram");
-		// giggs.create_context("Supported networks", "connectivity", "supported_networks");
-		// giggs.create_context("Weight", "dimensions", "weight");
 	}
 
 	if(query_status === 201 || query_status === 202){
@@ -160,122 +51,90 @@ function comparison_request(zlatan, query_status){
 
 		function find_attribute(element, index, array){
 
-		console.log("Attribute requested is: ", element);
+			console.log("Attribute requested is: ", element);
 
-		if(element === 'camera'){
+			if(element === 'camera'){
 
-			giggs.create_context("Primary camera", "camera", "primary_camera_pixels");
-			giggs.create_context("Selfie camera", "camera", "secondary_camera_pixels");
-			giggs.create_context("Video recording", "camera", "video_recording_status");
-			giggs.create_context("Video recording resolution", "camera", "video_recording_resolution");
-			giggs.create_context("HD Recording", "camera", "hd_recording_status");
-			giggs.create_context("Full HD Recording", "camera", "full_hd_recording_status");
-			giggs.create_context("Flash", "camera", "flash");
+				giggs.camera_element();
 
-		}
+			}
 
-		if(element === 'battery'){
+			if(element === 'battery'){
 
-			giggs.create_context("Battery capacity", "battery_power", "battery_capacity");
-			giggs.create_context("Battery type", "battery_power", "battery_type");
-			giggs.create_context("Removable battery status", "battery_power", "removable_battery_status");
+				giggs.battery_element();
 
-		}
+			}
 
-		if(element === 'sim card'){
+			if(element === 'sim card'){
 
-			giggs.create_context("Sim type", "sim_features", "sim_type");
-			giggs.create_context("Sim size", "sim_features", "sim_size");
-			giggs.create_context("Hybrid Sim status", "sim_features", "hybrid_sim_slot_status");
+				giggs.sim_element();
 
-		}
+			}
 
-		if(element === 'display'){
+			if(element === 'display'){
 
-			giggs.create_context("Screen size", "display_specifications", "display_size");
-			giggs.create_context("Display type", "display_specifications", "display_type");
-			giggs.create_context("Display colors", "display_specifications", "display_colors");
-			giggs.create_context("Resolution", "display_specifications", "resolution");
-			giggs.create_context("Resolution type", "display_specifications", "resolution_type");
+				giggs.display_element();
 
-		}
+			}
 
-		if(element === 'operating system'){
+			if(element === 'operating system'){
 
-			giggs.create_context("Operating system", "os_processor", "operating_system");
-			giggs.create_context("Processor speed", "os_processor", "processor_speed");
-			giggs.create_context("Processor core", "os_processor", "processor_core");
-			giggs.create_context("Processor type", "os_processor", "processor_type");
+				giggs.os_element();
 
-		}
+			}
 
-		if(element === 'processor'){
+			if(element === 'processor'){
 
-			giggs.create_context("Processor speed", "os_processor", "processor_speed");
-			giggs.create_context("Processor core", "os_processor", "processor_core");
-			giggs.create_context("Processor type", "os_processor", "processor_type");
+				giggs.processor_element();
 
-		}
+			}
 
-		if(element === 'RAM'){
+			if(element === 'RAM'){
 
-			giggs.create_context("RAM", "memory_storage", "ram");
+				giggs.ram_element();
 
-		}
+			}
 
-		if(element === 'connectivity'){
+			if(element === 'connectivity'){
 
-			giggs.create_context("Supported networks", "connectivity", "supported_networks");
-			giggs.create_context("Wifi", "connectivity", "wifi");
-			giggs.create_context("Bluetooth", "connectivity", "bluetooth_support");
-			giggs.create_context("Bluetooth version", "connectivity", "bluetooth_version");
-			giggs.create_context("USB connectivity", "connectivity", "usb_connectivity");
-			giggs.create_context("Audio jack", "connectivity", "audio_jack_width");
-			giggs.create_context("Near Field Communication (NFC)", "connectivity", "nfc");
+				giggs.connectivity_element();
+			}
 
-		}
+			if(element === 'bluetooth'){
 
-		if(element === 'bluetooth'){
+				giggs.bluetooth_element();
 
-			giggs.create_context("Bluetooth", "connectivity", "bluetooth_support");
-			giggs.create_context("Bluetooth version", "connectivity", "bluetooth_version");
+			}
 
-		}
+			if(element === 'nfc'){
 
-		if(element === 'nfc'){
+				giggs.nfc_element();
 
-			giggs.create_context("Near Field Communication (NFC)", "connectivity", "nfc");
+			}
+
+			if(element === 'jack'){
+
+				giggs.jack_element();
+
+			}
+
+			if(element === 'sensors'){
+
+				giggs.sensors_element();
+			}
+
+			if(element === 'warranty'){
+
+				giggs.warranty_element():
+
+			}
+
+			if(element === 'dimensions'){
+
+				giggs.dimensions_element();
+			}
 
 		}
-
-		if(element === 'jack'){
-
-			giggs.create_context("Audio jack", "connectivity", "audio_jack_width");
-
-		}
-
-		if(element === 'sensors'){
-
-			giggs.create_context("Sensors", "sensors");
-
-		}
-
-		if(element === 'warranty'){
-
-			giggs.create_context("Warranty", "warranty");
-
-		}
-
-		if(element === 'dimensions'){
-
-			giggs.create_context("Weight", "dimensions", "weight");
-			giggs.create_context("Width", "dimensions", "width");
-			giggs.create_context("Height", "dimensions", "height");
-			giggs.create_context("Depth", "dimensions", "depth");
-
-		}
-
-	}
 	}
 
 	zlatan.comparison_context = {
