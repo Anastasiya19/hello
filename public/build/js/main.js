3bd0b478a0fc3f448ec7e9bc9b9f6f3196440d03
Ankur Dhamija <ankurdhamija83@gmail.com>
2017-12-28 23:50:47 +0530

    Updated suggested questions element

--- public/build/js/main.js
+++ public/build/js/main.js
@@ -17779,16 +17779,21 @@ function batman_false(reply_received) {
         var text_reply = reply_received.web_reply.speech;
         $('.chat__messages')
             .append(`<div class="message message_vinci">
-            <div class="message__item message__item_vinci br-chat">
-              <p>${reply_received.web_reply.speech}</p>
+              <div class="message__item message__item_vinci br-chat">
+                <p>${reply_received.web_reply.speech}</p>
+              </div>
             </div>
-            <ul class="suggestion">
-              <li class="suggestion__item br-chat" onclick="send_question(event)">iPhone 8</li>
-              <li class="suggestion__item br-chat" onclick="send_question(event)">Best phones under 15000</li>
-              <li class="suggestion__item br-chat" onclick="send_question(event)">Best phones under 25000</li>
-              <li class="suggestion__item br-chat" onclick="send_question(event)">Best camera phones</li>
-              <li class="suggestion__item br-chat" onclick="send_question(event)">Light weight phones</li>
-            </ul>
+            <div class="message message_vinci">
+              <div>
+                <ul class="suggestion owl-carousel owl-theme">
+                  <li class="suggestion__item br-chat" onclick="send_question(event)">iPhone 8</li>
+                  <li class="suggestion__item br-chat" onclick="send_question(event)">Best phones under 15000</li>
+                  <li class="suggestion__item br-chat" onclick="send_question(event)">Best phones under 25000</li>
+                  <li class="suggestion__item br-chat" onclick="send_question(event)">Best camera phones</li>
+                  <li class="suggestion__item br-chat" onclick="send_question(event)">Light weight phones</li>
+                </ul>
+              </div>
+            </div>
           </div>`);
 
         $('.chat__messages').scrollTop($('.chat__messages')[0].scrollHeight);
@@ -17809,16 +17814,21 @@ function batman_missing(reply_received) {
         var text_reply = reply_received.web_reply.speech;
         $('.chat__messages')
             .append(`<div class="message message_vinci">
-            <div class="message__item message__item_vinci br-chat">
-              <p>There seems to be an issue at my end. Please try another query.</p>
+              <div class="message__item message__item_vinci br-chat">
+                <p>${reply_received.web_reply.speech}</p>
+              </div>
             </div>
-            <ul class="suggestion">
-              <li class="suggestion__item br-chat" onclick="send_question(event)">iPhone 8</li>
-              <li class="suggestion__item br-chat" onclick="send_question(event)">Best phones under 15000</li>
-              <li class="suggestion__item br-chat" onclick="send_question(event)">Best phones under 25000</li>
-              <li class="suggestion__item br-chat" onclick="send_question(event)">Best camera phones</li>
-              <li class="suggestion__item br-chat" onclick="send_question(event)">Light weight phones</li>
-            </ul>
+            <div class="message message_vinci">
+              <div>
+                <ul class="suggestion owl-carousel owl-theme">
+                  <li class="suggestion__item br-chat" onclick="send_question(event)">iPhone 8</li>
+                  <li class="suggestion__item br-chat" onclick="send_question(event)">Best phones under 15000</li>
+                  <li class="suggestion__item br-chat" onclick="send_question(event)">Best phones under 25000</li>
+                  <li class="suggestion__item br-chat" onclick="send_question(event)">Best camera phones</li>
+                  <li class="suggestion__item br-chat" onclick="send_question(event)">Light weight phones</li>
+                </ul>
+              </div>
+            </div>
           </div>`);
 
         $('.chat__messages').scrollTop($('.chat__messages')[0].scrollHeight);
