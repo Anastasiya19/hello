603a71594fce0e202f6778f20673e3f45f2f8286
Ankur Dhamija <ankurdhamija83@gmail.com>
2017-12-29 00:52:55 +0530

    Updated animation

--- client/src/js/app/batman_false.js
+++ client/src/js/app/batman_false.js
@@ -8,7 +8,7 @@ function batman_false(reply_received) {
         var text_reply = reply_received.web_reply.speech;
         $('.chat__messages')
             .append(`<div class="message message_vinci">
-              <div class="message__item message__item_vinci br-chat">
+              <div class="message__item message__item_vinci br-chat animated fadeInLeft">
                 <p>${reply_received.web_reply.speech}</p>
               </div>
             </div>
--- public/build/js/main.js
+++ public/build/js/main.js
@@ -17779,7 +17779,7 @@ function batman_false(reply_received) {
         var text_reply = reply_received.web_reply.speech;
         $('.chat__messages')
             .append(`<div class="message message_vinci">
-              <div class="message__item message__item_vinci br-chat">
+              <div class="message__item message__item_vinci br-chat animated fadeInLeft">
                 <p>${reply_received.web_reply.speech}</p>
               </div>
             </div>
@@ -17814,7 +17814,7 @@ function batman_missing(reply_received) {
         var text_reply = reply_received.web_reply.speech;
         $('.chat__messages')
             .append(`<div class="message message_vinci">
-              <div class="message__item message__item_vinci br-chat">
+              <div class="message__item message__item_vinci br-chat animated fadeInLeft">
                 <p>${reply_received.web_reply.speech}</p>
               </div>
             </div>
